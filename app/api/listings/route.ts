ffd9829719dab9e12177c49aa8c4966b90010689
Ashish Pal <118504077+AshishPal8@users.noreply.github.com>
2024-08-31 09:43:56 +0530

    map country

---
+++ app/api/listings/route.ts
@@ -0,0 +1,28 @@
+import prisma from '@/app/libs/prismadb'
+import getCurrentUser from '@/app/actions/getCurrentUser'
+import { NextResponse } from 'next/server';
+
+export async function POST(request: Request){
+    const currentUser = await getCurrentUser();
+
+    if(!currentUser){
+        return NextResponse.error()
+    }
+
+    const body = await request.json();
+    const {
+        title,
+        description,
+        imageSrc, 
+        category, 
+        roomCount, 
+        bathroomCount, 
+        guestCount, 
+        location,
+        price
+    } = body;
+
+    Object.keys(body)
+
+
+}
