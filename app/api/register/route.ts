7ab8a64dfc466d82f6d3b8909cbf6f488602ce77
Ashish Pal <ashish81301669@gmail.com>
2024-09-01 07:31:57 +0530

    modify the register route

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -6,6 +6,16 @@ import { NextResponse } from 'next/server'
 export async function POST(request: Request){
     const {name, email, password} = await request.json()
 
+    const existingUser = await prisma.user.findUnique({
+        where: {
+            email: email
+        }
+    })
+
+    if(existingUser){
+        return NextResponse.json("Email already in use", {status: 401})
+    }
+
     const hashedPassword = await bcrypt.hash(password, 12)
 
     const user = await prisma.user.create({
